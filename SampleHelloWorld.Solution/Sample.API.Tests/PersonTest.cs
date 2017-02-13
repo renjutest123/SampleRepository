e4b5e53ddd57874c65327270a09c36a4ee478bfc
renjutest123 <renjujob.net@gmail.com>
2017-02-12 23:09:10 -0600

    intial commit
    intial commit

---
+++ SampleHelloWorld.Solution/Sample.API.Tests/PersonTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.API.Controllers;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Results;
+using System.Collections.Generic;
+using Sample.API.Models;
+
+namespace Sample.API.Tests
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void TestGetAllPersons()
+        {            
+            var personscontroller = new PersonsController();
+            var actionResult = personscontroller.GetAllPersons();
+            var response = actionResult as OkNegotiatedContentResult<IEnumerable<Person>>;
+            Assert.IsNotNull(response);
+            
+
+
+
+            // Act
+            //var response = controller.Get(10);
+
+            // Assert
+            //Product product;
+            // Assert.IsTrue(response.TryGetContentValue<Product>(out product));
+            // Assert.AreEqual(10, product.Id);
+
+        }
+
+        [TestMethod]
+        public void TestGetPerson()
+        {
+            var personscontroller = new PersonsController();
+            var actionResult = personscontroller.GetPerson(1);
+            var retrievedID = ((OkNegotiatedContentResult<Person>)actionResult).Content.ID;
+            Assert.AreEqual(1, retrievedID);
+        }
+    }
+}
---
+++ SampleHelloWorld.Solution/Sample.API/Controllers/PersonsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Sample.API.Models;
+
+namespace Sample.API.Controllers
+{
+    public class PersonsController : ApiController
+    {
+        //This can be read from a database if needed. 
+        Person[] persons = new Person[]
+        {
+            new Person { ID = 1, FirstName = "Jane", LastName = "Doe" },
+            new Person { ID = 1, FirstName = "John", LastName = "Smith" }            
+        };
+
+        public IHttpActionResult GetAllPersons()
+        {
+            return Ok(persons.AsEnumerable());            
+        }
+
+        public IHttpActionResult GetPerson(int ID)
+        {
+            var person = persons.FirstOrDefault(p => p.ID == ID);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+    }
+}
